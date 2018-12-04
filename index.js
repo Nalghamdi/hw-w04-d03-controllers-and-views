ea8597aaed7d4dba341e863c4bc1e5360ee6d5f9
nalghamdi <nalghamdi_@hotmail.com>
2018-12-04 23:31:45 +0300

    hw done

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mustache = require('mustache-express');
+var app = express();
+var port = 3000;
+
+app.engine('html',mustache());
+app.set('view engine','html')
+app.set('views',__dirname + '/views');
+
+
+var angryBirdsController = require('./controllers/angryBirds_controller');
+
+
+app.get('/', function(request, response){
+response.render("./index.html");
+})
+
+app.use('/angry_birds', angryBirdsController);
+
+
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port}`);
+})
+
